ed9ab7f5e8dc85ab05eeee3c9a5213ccb18269f5
alexis <garbuzov.boris@gmail.com>
2017-06-15 15:34:21 +0300

    initial implementation of computeALlRhoHats.R; improving docs

---
+++ SCB/R/createTPar.R
@@ -0,0 +1,14 @@
+#' Create \code{T parameter}.
+#'
+#' @aliases createSleeper
+#' @param number_steps the desired length of \code{T parameter} vector.
+#' @return The vector of \code{sleeper} values
+#' @examples
+#' createSleeper(number_steps = 10)
+#' createSleeper(number_steps = 100)
+
+createTPar <- function(number_steps) {
+  number_steps <- seq(from = 0,
+           to = 1-1/number_steps,
+           by = 1/number_steps)
+}
