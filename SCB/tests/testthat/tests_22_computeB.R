3567f9092963f184fa7f94e5df248836a3aa266f
Oksana789 <palidrome789@gmail.com>
2017-07-23 13:03:51 +0300

    add test for computeB

---
+++ SCB/tests/testthat/tests_22_computeB.R
@@ -0,0 +1,15 @@
+computeBFunction <- function() {
+  cat ("\n Testing \'computeB\' for for being numeric for values from 1 to 5\n")
+  myN = c(1:5)
+  mockB <- computeB(n = myN)
+  cat ("mockB[1:5] =", mockB[1:5], "\n")
+
+  # The actual testing
+  expect_that(mockB, is_a("numeric"))
+}
+
+test_that("Testing \'computeB\' for being an array", {
+  mockB <- computeBFunction()
+}
+)
+
