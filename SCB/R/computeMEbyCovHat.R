68ba7492c6b7c46199e975606047f70b947292ba
Gograman <gograman93@gmail.com>
2017-07-27 16:02:36 +0300

    createME

--- SCB/R/computeMEbyCovHat.R
+++ SCB/R/computeMEbyCovHat.R
@@ -20,28 +20,36 @@ computeMEbyCovHat <- function (
   lag,
   sample,
   bandwidth,
-  nonCoverageProbability
+  alpha
   )
 {
   # Array rho_h(i),
   # counter i corresponds to t_i = 0, 1/n, …., 1 - 1/n,
   # horizontal ACF of a sleepers process
-  lengthSleeper <- length(sleeper)
+ #lengthSleeper <- length(sleeper)
   # do we need this?
   # rhoHI <- rep(0, 10)
   CK = -1.978325
   INT_SQ_DER = 0.306951
   PHI_K_NORMAL_DIF = 0.4065
+# mylag=2
+ mytParArray=createTParArray(tParCount = 10)
 
 
+
+
+
+mykernel=normalDifferenceKernel
   myBetaLRVHat = computeBetaLRVHat(
-    tParArray,
+   tParArray=mytParArray,
     lag = lag,
-    sample = sample,
-    kernel = normalDifferenceKernel,
-    bandwidth = bandwidth
-  )
-  MySqrt = sqrt (- 2 * log (bandwidth))
+    sample =sample,
+    kernel = mykernel,
+    bandwidth = bandwith)
+  sampleSize=8
+  MySqrt = sqrt (-2 * log (bandwidth))
   CFactor = MySqrt  + (CK - log (log (1/ sqrt (1 - alpha)))) / MySqrt
+ ME=CFactor*myBetaLRVHat*sqrt (PHI_K_NORMAL_DIF/(sampleSize*bandwidth))
+ ME
 }
 
