af49bfa93424728cfe8d1212dfa4c690cc259537
Gograman <gograman93@gmail.com>
2017-07-27 16:21:11 +0300

    change

--- SCB/R/computeMEbyCovHat.R
+++ SCB/R/computeMEbyCovHat.R
@@ -16,40 +16,44 @@
 
 
 
-computeMEbyCovHat <- function (
-  lag,
-  sample,
-  bandwidth,
-  alpha
-  )
+computeMEbyCovHat <- function (lag,
+                               sample,
+                               bandwidth,
+                               alpha)
 {
   # Array rho_h(i),
   # counter i corresponds to t_i = 0, 1/n, …., 1 - 1/n,
   # horizontal ACF of a sleepers process
- #lengthSleeper <- length(sleeper)
+  #lengthSleeper <- length(sleeper)
   # do we need this?
   # rhoHI <- rep(0, 10)
   CK = -1.978325
   INT_SQ_DER = 0.306951
   PHI_K_NORMAL_DIF = 0.4065
-# mylag=2
- mytParArray=createTParArray(tParCount = 10)
+  # mylag=2
+  mytParArray = createTParArray(tParCount = 10)
 
 
 
+  mockAllCorHats <- computeAllCorHats(tParArray = mockTParArray,
+                                      lagCount = myLagCount,
+                                      sample = mockSample,
+                                      kernel = myKernel,
+                                      bandwidth = myBandwidth)
 
-
-mykernel=normalDifferenceKernel
+  mykernel = normalDifferenceKernel
   myBetaLRVHat = computeBetaLRVHat(
-   tParArray=mytParArray,
+    tParArray = mytParArray,
     lag = lag,
-    sample =sample,
+    sample = sample,
     kernel = mykernel,
-    bandwidth = bandwith)
-  sampleSize=8
+    bandwidth = bandwith,
+    allCorHats=mockAllCorHats
+  )
+  sampleSize = 8
   MySqrt = sqrt (-2 * log (bandwidth))
-  CFactor = MySqrt  + (CK - log (log (1/ sqrt (1 - alpha)))) / MySqrt
- ME=CFactor*myBetaLRVHat*sqrt (PHI_K_NORMAL_DIF/(sampleSize*bandwidth))
- ME
-}
+  CFactor = MySqrt  + (CK - log (log (1 / sqrt (1 - alpha)))) / MySqrt
 
+  ME = CFactor * myBetaLRVHat * sqrt (PHI_K_NORMAL_DIF / (sampleSize * bandwidth))
+  ME
+}
