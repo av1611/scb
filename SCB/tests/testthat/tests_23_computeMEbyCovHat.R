fbf9f2e48117bdad7d8de49784d0d3c7d36217fc
statistics101 <garbuzov.boris@gmail.com>
2017-07-28 09:15:27 -0400

    Update tests_23_computeMEbyCovHat.R

--- SCB/tests/testthat/tests_23_computeMEbyCovHat.R
+++ SCB/tests/testthat/tests_23_computeMEbyCovHat.R
@@ -1,4 +1,4 @@
-computeMEbyCovHat = function()
+computeMEFunction = function()
 {
   cat ("\n Testing \'computeME\'\n")
   myLag=2
@@ -22,7 +22,7 @@ mockTParArray <- createTParArray(tParCount = tParCount)
 
 }
 test_that("Testing \'computeME\' ", {
-  computeMEbyCovHat()
+  computeMEFunction()
 
 }
 )
