ecea9f140d5e5dead69e892b5e4c273dddb49fb2
Gograman <gograman93@gmail.com>
2017-08-04 14:31:17 +0300

    create new algoritm and func

--- SCB/R/computeNonCoverageFreqByRhoBands.R
+++ SCB/R/computeNonCoverageFreqByRhoBands.R
@@ -28,9 +28,33 @@ computeNonCoverageFreqByRhoBands  <- function(bandsArray,
   # Calculates alpha hat or
   # nonCoverageProbabilityHat = number of zeros / superReplication
   # nonCoverageProbabilityHat <-
-  n=length(bandsArray);
+  array1=array(0,dim=c(replicationCount,samplesize,2))
+
+  n=length(bandsArray)
   for(index in 1:n)
   {
+   # band=createBand(X = rnorm(n = 10, mean = 0, sd = 1), nonCoverageProbability = 0.05)
+    myLag=2
+    tParCount = 10
+    mockTParArray <- createTParArray(tParCount = tParCount)
+    mockNoise <- createNoise(sampleSize = tParCount,
+                             mean = 0,
+                             sd = 1)
+    mockTVMA1CoeffArray <- createTVMA1CoefArray(coefFunction = sin,
+                                                tParArray = mockTParArray)
+    mockTVMA1 <- createTVMA1(sampleSize = tParCount,
+                             tvMA1CoefArray = mockTVMA1CoeffArray,
+                             noise = mockNoise)
+    Cor=computeCor(lag=myLag,tvMa1CoefArray = mockTVMA1)
+    zeroCount=0
+    IsCovered=computeIsCovered(bandsArray[i],Cor)
+    replicationCount=n
+    for(i in 1:length(IsCovered))
+    {
+      if(IsCovered[i]==0)
+        zeroCount=zeroCount+1
+    }
+    nonCoverageProbability=zeroCount/replicationCount
 
 
   }
