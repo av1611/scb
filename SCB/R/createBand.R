add46fd861a8d7c12abfcd2819d09ec439b8aa79
statistics101 <garbuzov.boris@gmail.com>
2017-08-07 09:51:10 -0400

    createBand

--- SCB/R/createBand.R
+++ SCB/R/createBand.R
@@ -17,38 +17,27 @@
 #' @examples
 #' createBand(X = rnorm(n = 10, mean = 0, sd = 1), alpha = 0.05))
 
-createBand <- function(lag,sample,
+createBand <- function(tParArray,
+                       lag,
+                       sample,
                        bandwidth = 1,
-                       nonCoverageProbability = 0.05) {
+                       kernel = normalDifferenceKernel,
+                       nonCoverageProbability = 0.05)
+{
   # get the real correlation computeCor
   # call ME (sample, bandwidth, nonCoverageProbability)
   # upper = corArray + ME
   # upper = corArray + ME
-
-
-  tParCount = 10
-  mockTParArray <- createTParArray(tParCount = tParCount)
-  mockTVMA1CoefArray <- createTVMA1CoefArray(coefFunction = sin,
-                                             tParArray = mockTParArray)
-  mockNoise <- createNoise(sampleSize = 10,
-                           mean = 0,
-                           sd = 1)
-  mockTVMA1Array <- createTVMA1CoefArray(coefFunction = sin,
-                                         tParArray = mockTParArray)
-  mockSample <- createSample(model = createTVMA1,
-                             tvMA1CoefArray = mockTVMA1Array)
-  myKernel = normalDifferenceKernel
-
-  mockCorHat <- computeCorHat(tParArray = mockTParArray,
-                              lag,
-                              sample = mockSample,
-                              kernel = myKernel,
-                              bandwidth )
-  meByCovHat <- computeMEbyCovHat(lag,
-                                  sample=mockSample,
-                                  bandwidth,
-                                  nonCoverageProbability=0.05)
-  lowerBound <- mockCorHat - meByCovHat
-  upperBound <- mockCorHat + meByCovHat
+  corHat <- computeCorHat(tParArray = tParArray,
+                              lag = lag,
+                              sample = sample,
+                              kernel = kernel,
+                              bandwidth = bandwidth)
+  meByCovHat <- computeMEbyCovHat(lag = lag,
+                                  sample = sample,
+                                  bandwidth = bandwidth,
+                                  nonCoverageProbability = nonCoverageProbability)
+  lowerBound <- corHat - meByCovHat
+  upperBound <- corHat + meByCovHat
   band <- rbind(lowerBound, upperBound)
 }
