d3b1154e60bd6edffd335a7fb1c93d411882d7df
Adlet <botahanov@aol.com>
2017-08-27 03:19:26 +0600

    some experiment with plots

--- lab/graphics/experimentWithLines.R
+++ lab/graphics/experimentWithLines.R
@@ -7,13 +7,21 @@ myY = 1 : 10
 plot (myY ~ myX)
 lines (myY ~ myX, col = "blue")
 
-rm (list = ls())
+
 x  <- seq(-2, 2, 0.05)
 y1 <- pnorm(x)
 y2 <- pnorm(x,1,1)
 plot(x,y1,type="l",col="red")
 lines(x,y2,type="l",col="green")
 
+rm (list = ls())
+lag <- 1
+sampleSize <- 200
 
+MyMA1CoefArray <- createTVMA1CoefArray(coefFunction = sin, sampleSize = sampleSize)
+correlation <- computeCor(lag = lag, tvMa1CoefArray = MyMA1CoefArray)
+t <- createTParArray(tParCount = sampleSize)
+plot(x = t,y = correlation, type="l")
 
+jpeg("mygraph.jpg")
 
