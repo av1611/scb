d94190042e796b2e9ce0bd5a1f67e2e94a61b1a3
Adlet <botahanov@aol.com>
2017-08-29 19:56:25 +0600

    Some graphics fixes

--- lab/graphics/experimentWithAlpha&AlphaHar.R
+++ lab/graphics/experimentWithAlpha&AlphaHar.R
@@ -7,7 +7,7 @@ if(!dir.exists(myPath))
 myFileName <- gsub(" ","_",paste("myGraphWithAlpha&AlphaHat_",Sys.time(),".jpg",sep = ""))
 myFileName <- gsub(":","_",myFileName)
 #jpeg(paste(myPath,"/",myFileName,sep=""))
-#dev.off()
+
 alpha <- 0.8
 alphaHat <- c(0.72,0.75,0.82,0.78,0.7,0.83)
 
@@ -17,3 +17,4 @@ plot(x=c(0,1),y=c(0,1),type ="n")
 abline(a = 0 , b =1)
 points(x=alpha, y=0,col="blue")
 points(x=arrayOfXAplphaHat,y=alphaHat,col="red")
+#dev.off()
