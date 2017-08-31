b5f57919dfea1c8b1a7ad9a93ae7c6ab1158220e
Gograman <gograman93@gmail.com>
2017-08-31 14:24:25 +0300

    nonCoverageArray working

--- SCB/tests/testthat/tests_32_computeNonCoverageFreqArray.R
+++ SCB/tests/testthat/tests_32_computeNonCoverageFreqArray.R
@@ -1,5 +1,7 @@
 
 
+
+
 computeNonCoverageFreqArrayFunction <- function() {
   cat("\n Testing \'computeNonCoverageFreqArray\' \n")
 
@@ -7,39 +9,31 @@ computeNonCoverageFreqArrayFunction <- function() {
   mockTParArray  <- createTParArray(tParCount = myTParCount)
   mySuperReplicationCount <- 4
   myReplicationCount <- 4
-  mySampleSize <- 5
+  mySampleSize <- 50
   myLag <- 1
   myLagCount <- 4
   myKernel <- normalDifferenceKernel
   myBandwidth <- 0.5
   myNonCoverageProbability <- 0.05
 
+  nonCoverageFreqArray = computeNonCoverageFreqArray(
+    superReplicationCount = mySuperReplicationCount,
+    replicationCount = myReplicationCount,
+    sampleSize = mySampleSize,
+    lag = myLag,
+    lagCount = myLagCount,
+    tParArray = mockTParArray,
+    kernel = myKernel,
+    bandwidth = myBandwidth,
+    nonCoverageProbability = myNonCoverageProbability
+  )
 
-
-  corArray <-
-    computeCor(lag = myLag,
-               coefFunction = sin,
-               tParArray = mockTParArray)
-
-
-  nonCoverageFreqArray <-
-    computeNonCoverageFreqArray(
-      superReplicationCount = mySuperReplicationCount,
-      replicationCount = myReplicationCount,
-      sampleSize = mySampleSize,
-      lag = myLag,
-      lagCount = myLagCount,
-      tParArray = mockTParArray,
-      kernel = myKernel,
-      bandwidth = myBandwidth,
-      nonCoverageProbability = myNonCoverageProbability
-    )
-
-  cat("NonCoverageFreqArray:", nonCoverageFreqDist, "\n")
-  cat("End of test of computeNonCoverageFreqDist", "\n")
+  cat("nonCoverageArray= ",nonCoverageFreqArray)
 
 }
 
 test_that("Testing \'computeNonCoverageFreqArray\'", {
   computeNonCoverageFreqArrayFunction()
+  cat("\nEnd of test computeNonCoverageFreqArray", "\n")
+  cat("=====================")
 })
